92f8e17d1a75a0085c87eace9ee6d805d9c24796
Tola Bello <tolabello4@gmail.com>
2023-02-07 04:29:26 -0800

    the javascript file2

--- js17/script17.js
+++ js17/script17.js
@@ -22,8 +22,32 @@ $(document).ready(function () {
     else {
       alert("you are to old");
     }
+    //celebritity dating site function2
+    $("#form-two").submit(function (event) {
+      event.preventDefault()
+      let job = $("input:radio[name=job]:checked").val();
+      let food = $("input:radio[name=food]:checked").val();
+      let hobbies = $("input:radio[name=hobbies]:checked").val();
 
-    
+      if (job === 'celeb1' && food === 'celeb4' && hobbies === 'celeb7' || job === 'celeb3' && food === 'celeb5' && hobbies === 'celeb9' || job === 'celeb2' && food === 'celeb6' && hobbies === 'celeb8') {
+        $("div#one").show();
+        $(".female").hide();
+      } else if (job === 'celeb2' && food === 'celeb5' && hobbies === 'celeb8' || job === 'celeb1' && food === 'celeb6' && hobbies === 'celeb9' || job === 'celeb3' && food === 'celeb4' && hobbies === 'celeb7') {
+        $("div#two").show();
+        $(".female").hide();
+      }
+      else if (job === 'celeb3' && food === 'celeb6' && hobbies === 'celeb9' || job === 'celeb2' && food === 'celeb4' && hobbies === 'celeb8' || job === 'celeb1' && food === 'celeb5' && hobbies === 'celeb7') {
+        $("div#three").show();
+        $(".female").hide();
+      }
+      else {
+        $("div#four").show();
+        $(".female").hide();
+      }
+
+    });
+
+
   });
 });
 
